4af976303a983407b606532a6abfaa24b3784d77
Jose Daniel Aley Jimenez <jaley@bp.fi.cr>
2023-09-28 16:42:07 -0600

    actualizacion pagin de servicios

--- js/index.js
+++ js/index.js
@@ -5,3 +5,53 @@ window.onload = function () {
         document.getElementById('menu-options').style.display = 'block';
     });
 }
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    try {
+        var circles = document.getElementsByClassName('number-black');
+        var text = document.getElementById('text-base');
+
+        for (var element in circles) {
+            var element2 = circles[element]
+            let rect = text.getBoundingClientRect();
+            element2.style.left = (rect.left - 50 - 17.815).toString() + 'px';
+        }
+
+        // document.getElementById('request_call').addEventListener('submit', (name,) => {
+
+        // });
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+window.visualViewport.addEventListener("resize", viewportHandler);
+function viewportHandler(event) {
+    try {
+        var circles = document.getElementsByClassName('number-black');
+        var text = document.getElementById('text-base');
+
+
+        for (var element in circles) {
+            var element2 = circles[element]
+            let rect = text.getBoundingClientRect();
+            element2.style.left = (rect.left - 45 - 17.815).toString() + 'px';
+        }
+    } catch (error) {
+    }
+}
+
+function clickAccordion(element) {
+    var nextElement = element.nextElementSibling;
+    if (element.classList.contains('active')) {
+        element.classList.remove('active');
+        nextElement.style.display = 'none';
+    } else {
+        element.classList.add('active');
+        nextElement.style.display = 'block';
+    }
+}

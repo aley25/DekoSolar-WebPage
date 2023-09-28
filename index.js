b80b2e9614ec66a31cc08a3e3aa5de8580a12f34
Jose Daniel Aley Jimenez <jaley@bp.fi.cr>
2023-09-28 13:55:25 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+window.onload = function () {
+    document.getElementById('menu-button').addEventListener('click', function (event) {
+        document.getElementById('menu-options').style.display = 'block';
+    });
+}
